ed52d4500aa7c95b52b2315c6d9153e3d5f4af6a
jayesh96 <jayesh.bidani@gmail.com>
2019-02-02 13:54:17 +0530

    new changes

--- src/containers/rightPanel/index.js
+++ src/containers/rightPanel/index.js
@@ -1,36 +1,41 @@
-import React, {Component, Fragment} from 'react';
-import './styles.css';
-import {connect} from 'react-redux';
-import {addMessage} from './actions';
-import ChatBox from '../chatBox'
-import '../../scripts/main';
+import React, { Component, Fragment } from "react";
+import "./styles.css";
+import { connect } from "react-redux";
+import { addMessage } from "./actions";
+import ChatBox from "../chatBox";
+import "../../scripts/main";
 
 class RightPanel extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      code: '',
-      message: '',
+      code: "",
+      message: "",
       messageCount: 0,
-      loading: false,
+      loading: false
     };
     this.messageSubmit = this.messageSubmit.bind(this);
   }
 
   componentWillReceiveProps(nextProps) {
-    this.setState({code: nextProps.code});
+    this.setState({ code: nextProps.code });
   }
 
   messageSubmit(e) {
     var code = e.keyCode ? e.keyCode : e.which;
     if (code === 13) {
-      this.setState({
-        messageCount: this.state.messageCount + 1,
-        message: '',
-        loading: true,
-      });
-      this.props.addMessage(e.target.value);
-      this.evaluateCode(this.state.code, e.target.value);
+      if (this.state.code.length === 0) {
+        alert("Please Pass a function");
+        this.setState({message:''})
+      } else {
+        this.setState({
+          messageCount: this.state.messageCount + 1,
+          message: "",
+          loading: true
+        });
+        this.props.addMessage(e.target.value);
+        this.evaluateCode(this.state.code, e.target.value);
+      }
     }
   }
 
@@ -41,41 +46,46 @@ class RightPanel extends Component {
         // eslint-disable-next-line
         let value = await eval(`(${code})(${message})`);
         this.props.addMessage(value);
-        this.setState({loading: false});
+        this.setState({ loading: false });
         return value;
       } else {
         const _message = message.replace(/"/g, "'");
         // eslint-disable-next-line
         let value = await eval(`(${code})("${_message}")`);
         this.props.addMessage(value);
-        this.setState({loading: false});
+        this.setState({ loading: false });
         return value;
       }
     } catch (err) {
-      this.props.addMessage('Oops! ' + err.toString());
-      this.setState({loading: false});
-      return 'NIL';
+      this.props.addMessage("Oops! " + err.toString());
+      this.setState({ loading: false });
+      return "NIL";
     }
   }
 
   render() {
-    const {messageCount, message, loading} = this.state;
-    const {messages} = this.props;
+    const { messageCount, message, loading } = this.state;
+    const { messages } = this.props;
     return (
       <Fragment>
-        <ChatBox code={this.props.code} messageCount={messageCount} messages={messages} loading={loading}/> 
-        <div className={'messageBox'}>
-          <div className={'Rectangle-3'}>
-            <div style={{width: '100%'}}>
+        <ChatBox
+          code={this.props.code}
+          messageCount={messageCount}
+          messages={messages}
+          loading={loading}
+        />
+        <div className={"messageBox"}>
+          <div className={"Rectangle-3"}>
+            <div style={{ width: "100%" }}>
               <input
                 type="text"
-                disabled={loading?true:false}
+                disabled={loading ? true : false}
                 value={message}
-                onChange={(e) => {
-                  this.setState({message: e.target.value});
+                onChange={e => {
+                  this.setState({ message: e.target.value });
                 }}
                 placeholder="Your Message Here"
-                className={'Type-message-here'}
+                className={"Type-message-here"}
                 onKeyPress={this.messageSubmit}
               />
             </div>
@@ -86,19 +96,22 @@ class RightPanel extends Component {
   }
 }
 
-const mapStateToProps = (state) => {
-  return {code: state.saveCode.get('active')['code'], messages: state.addMessage};
+const mapStateToProps = state => {
+  return {
+    code: state.saveCode.get("active")["code"],
+    messages: state.addMessage
+  };
 };
 
-const mapDispatchToProps = (dispatch) => {
+const mapDispatchToProps = dispatch => {
   return {
-    addMessage: (payload) => {
+    addMessage: payload => {
       dispatch(addMessage(payload));
-    },
+    }
   };
 };
 
 export default connect(
   mapStateToProps,
-  mapDispatchToProps,
+  mapDispatchToProps
 )(RightPanel);
